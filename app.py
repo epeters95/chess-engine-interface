97c8940e30cf023f1402dc0d5952839edf24b3b8
Erik <ewpeters@asu.edu>
2025-06-18 21:53:08 -0700

    enable ponder

--- app.py
+++ app.py
@@ -155,7 +155,7 @@ def default_settings():
     "Min Split Depth": 0,
     "Threads": 1,
     # More threads will make the engine stronger, but should be kept at less than the number of logical processors on your computer.
-    "Ponder": "false",
+    "Ponder": "true",
     "Hash": 64,
     # Default size is 16 MB. It's recommended that you increase this value, but keep it as some power of 2. E.g., if you're fine using 2 GB of RAM, set Hash to 2048 (11th power of 2).
     "MultiPV": 1,
